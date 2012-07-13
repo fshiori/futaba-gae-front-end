b148d16d9ee325469840802a92487abc72d027cb
fshiori@gmail.com <fshiori@gmail.com@965a12de-1564-1985-bf4d-9a30f960c65f>
2012-07-13 11:30:14 +0000

    1. add memcache

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 import webapp2
+from google.appengine.api import memcache
 
 from urls import routes
 from settings import DEBUG, config
 from handler import BaseHandler
 from models import Flatpage, Category, Board
-
-def _debug_flatpage():
-    flatpage = Flatpage(name='MOTD', content='123')
-    flatpage.put()
-    category = Category(name='Test')
-    category.put()
-    board = Board(category=category, name='Test', link='http://www.yahoo.com.tw')
-    board.put()
 
 def get_flatpage(title):
     q = Flatpage.all()
@@ -23,7 +16,11 @@ def get_flatpage(title):
 
 class IndexPage(BaseHandler):
     def get(self):
-        #_debug_flatpage()
+        if config['MEMCAHCE_ENABLE']:
+            data = memcache.get('IndexPage')
+            if data is not None:
+                self.response.write(data)
+                return
         context = {}
         context['config'] = config
         context['content'] = ''
@@ -32,24 +29,48 @@ class IndexPage(BaseHandler):
             return
         res = get_flatpage('MOTD')
         if res : context['content'] = res
-        self.render_response('index.html', **context)
+        data = self.render_to_cahce('index.html', **context)
+        memcache.add('IndexPage', data, config['MEMCACHE_EXPIRES'])
+        self.response.write(data)
+        #self.render_response('index.html', **context)
         
 class MainPage(BaseHandler):
     def get(self):
+        if config['MEMCAHCE_ENABLE']:
+            data = memcache.get('MainPage')
+            if data is not None:
+                self.response.write(data)
+                return
         context = {}
         context['config'] = config
-        self.render_response('main.html', **context)
+        data = self.render_to_cahce('main.html', **context)
+        memcache.add('MainPage', data, config['MEMCACHE_EXPIRES'])
+        self.response.write(data)
+        #self.render_response('main.html', **context)
         
 class MotdPage(BaseHandler):
     def get(self):
+        if config['MEMCAHCE_ENABLE']:
+            data = memcache.get('MotdPage')
+            if data is not None:
+                self.response.write(data)
+                return
         context = {}
         context['config'] = config
         res = get_flatpage('MOTD')
         if res : context['content'] = res
-        self.render_response('main_motd.html', **context)
+        data = self.render_to_cahce('main_motd.html', **context)
+        memcache.add('MotdPage', data, config['MEMCACHE_EXPIRES'])
+        self.response.write(data)
+        #self.render_response('main_motd.html', **context)
         
 class MenuPage(BaseHandler):
     def get(self):
+        if config['MEMCAHCE_ENABLE']:
+            data = memcache.get('MenuPage')
+            if data is not None:
+                self.response.write(data)
+                return
         context = {}
         context['config'] = config
         q = Category.all()
@@ -57,6 +78,19 @@ class MenuPage(BaseHandler):
         q.order('order')
         q.fetch(1000)
         context['categorys'] = q
-        self.render_response('bbsmenu.html', **context)
+        data = self.render_to_cahce('bbsmenu.html', **context)
+        memcache.add('MenuPage', data, config['MEMCACHE_EXPIRES'])
+        self.response.write(data)
+        #self.render_response('bbsmenu.html', **context)
         
+class ClearCachePage(BaseHandler):
+    def get(self):
+        if config['MEMCAHCE_ENABLE']:
+            memcache.delete('IndexPage')
+            memcache.delete('MainPage')
+            memcache.delete('MotdPage')
+            memcache.delete('MenuPage')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('okay!')
+      
 app = webapp2.WSGIApplication(routes=routes, debug=DEBUG, config=config)
